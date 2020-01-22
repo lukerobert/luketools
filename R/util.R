267eb16e78d77bc6a621ac8ca9868f33ee6b64d6
Luke Davis <luke@Lukes-MacBook-Pro.local>
2020-01-22 11:40:50 -0500

    Cleaning up non-functional utils for merge

--- R/util.R
+++ R/util.R
@@ -145,7 +145,7 @@ parallelize <- function(expr, num_cores = NULL, run_time = TRUE, ...) {
 #'   and `pmax`.
 #'
 #' A more general way to approach this type of problem is with
-#'   `purrr::lift`.
+#'   `[purrr::lift()]`.
 #' @export
 pmean <- function(..., na.rm = TRUE) {
   inputs <- list(...)
@@ -203,71 +203,6 @@ balance_by <- function(data, var) {
     dplyr::bind_rows(least_common_value_data)
 }
 
-#' Chop off URL subdomains and directories
-#'
-#' @param url Vector of URLs to process
-#' @param n_dirs Number of directories (e.x., lukedav.is/_blog_) to keep.
-#'   Pass `NULL` (the default) to keep all and 0 to remove all.
-#' @param n_subdomains Number of subdomains (e.x., _blog_.lukedav.is) to keep.
-#'   #'   Pass `NULL` (the default) to keep all and 0 to remove all.
-#' @param show_regex Should the generated regex be printed? This can be useful
-#'   if you want to repeat the same "chopping" in another system that supports
-#'   regex, like a SQL query.
-#' @details For more general URL extraction tasks check out the urltools package.
-#'   This function should _not_ be used to verify if an input is a valid URL;
-#'   Its rules for TLDs (e.x., ".com", ".org") are extremely generous and it
-#'   isn't intended to validate the URL format more broadly.
-#' @return A vector of URLs the same length as the input with directories
-#'   and / or subdomains truncated.
-#' @export
-chop_url <- function(url, n_dirs = 1, n_subdomains = 1, show_regex = FALSE) {
-  protocol <- "(?:https?:\\/\\/)?(?:www\\.)?"
-  subdomain_start <- "((?:[^\\.]+\\.)"  # Domains above the root
-  subdomain_end <- ")?"
-  if (is.null(n_subdomains) || n_subdomains == 0) {
-    subdomain_rep <- "*"
-  } else if (n_subdomains == 1) {
-    subdomain_rep <- "{{{n_subdomains}}}"
-  } else {
-    subdomain_rep <- "{{1,{n_subdomains}}}"
-  }
-  root_domain <- "([^\\.]+\\.(?:org|com))"
-  dir_start <- "((?:/[^/\\?]*)"  # Directories past the domain
-  dir_end <- ")?"
-  if (is.null(n_dirs) || n_dirs == 0) {
-    dir_rep <- "*"
-  } else if (n_dirs == 1) {
-    dir_rep <- "{{{n_dirs}}}"
-  } else {
-    dir_rep <- "{{1,{n_dirs}}}"
-  }
-  # Compile full regex pattern string.
-  pattern <- glue::glue(
-    protocol,
-    subdomain_start, subdomain_rep, subdomain_end,
-    root_domain,
-    dir_start, dir_rep, dir_end,
-    n_dirs = n_dirs,
-    n_subdomains = n_subdomains
-  )
-  if (show_regex) print(pattern)
-  full_match <- stringr::str_match(url, pattern = pattern)
-  if (is.na(full_match[, 1])) return(NA)
-  # Extract constituent parts of match and reconstruct "chopped" url.
-  subdomains <- full_match[, 2]
-  match <- full_match[, 3]
-  dirs <- full_match[, 4]
-  if (!is.null(n_subdomains) && n_subdomains > 0) {
-    if (is.na(subdomains)) subdomains <- ""
-    match <- paste0(subdomains, match)
-  }
-  if (!is.null(n_dirs) && n_dirs > 0) {
-    if (is.na(dirs)) dirs <- ""
-    match <- paste0(match, dirs)
-  }
-  match
-}
-
 #' Find first or last time condition was met
 #'
 #' This function is designed to be used as part of dplyr pipelines.
@@ -287,11 +222,6 @@ last_time <- function(cond, time_col) {
   suppressWarnings(
     dplyr::if_else(cond, {{time_col}}, as.POSIXct(NA)) %>% max(na.rm = TRUE)
   )
-}
-
-prop <- function(df, count_var = "n", name = "prop") {
-  # TODO: test
-  dplyr::mutate(df, {{name}} := {{count_var}} / sum({{count_var}}))
 }
 
 #' Extract a single match group
@@ -331,6 +261,21 @@ extract_url_param <- function(url, param) {
   stringr::str_detect(string = string, pattern = pattern)
 }
 
+#' Check if elements are between two values
+#'
+#' This is a convenience syntax for the common pattern of `x >= y & x <= z`.
+#' The right hand side can be supplied as a simple vector: e.x., `x %between% c(y, z)`.
+#' The min and max of the right hand side vector are used for the comparison, so
+#' the order of the elements in the vector doesn't matter. This also means you
+#' could use the function to succinctly find whether or not an element falls
+#' within the range of another vector.
+#'
+#' @return A logical vector indicating whether each element of the left hand
+#' side was between the min and max of the right hand side.
+#' @details Both sides of the comparison are inclusive. For example,
+#' `1 %between% c(1, 3)` evaluates to `TRUE`, and so does `3 %between% c(1, 3)`.
+#'
+#' @export
 `%between%` <- function(x, y) {
-  x >= y[1] & x <= y[2]
+  x >= min(y) & x <= max(y)
 }
